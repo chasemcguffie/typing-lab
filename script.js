934522fd2d874ccd8eafce130d21772125fde8fc
chasemcguffie <chasemcguffie@gmail.com>
2018-01-29 15:32:04 -0600

    actual bugfixes

--- script.js
+++ script.js
@@ -5,13 +5,22 @@ var sentences = ['ten ate neite ate nee enet ite ate inet ent eate',
 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
 let sentenceCounter = 0;
 let characterCounter = 0;
+let splitSentence = sentences[sentenceCounter].split('');
+numberOfWords = sentences[0].length + sentences[1].length + sentences[2].length + sentences[3].length + sentences[4].length;
+numberOfMistakes = 0;
+const wordsPerMinute = numberOfWords / minutes - 2 * numberOfMistakes
 
+
+var displaySentence = splitSentence.map((character, index) => {
+    return `<span id="target-${index}">${character}</span>`;
+}).join('');
 
 $(document).ready(function () {
 let $upper = $('#keyboard-upper-container')
 let $lower = $('#keyboard-lower-container')
 let shifted = false;
-
+$('#glyph').append('<p><span class="glyphicon glyphicon-thumbs-up"></span></p>');
+$('#glyph').append('<p><span class="glyphicon glyphicon-thumbs-down"></span></p>');
 $('#target-0').css('background-color', 'yellow')
 $('#keyboard-upper-container').hide();
 console.log(shifted)
@@ -31,24 +40,41 @@ $(document).keydown(function( event ) {
             console.log(shifted);
         }
     });
-    let splitSentence = sentences[sentenceCounter].split('');
-
+    const nextSentence = () => {
     var displaySentence = splitSentence.map((character, index) => {
         return `<span id="target-${index}">${character}</span>`;
     }).join('');
-    if (characterCounter == displaySentence.length) {
-        $('#sentence').empty();
-        sentenceCounter = sentenceCounter + 1;
-        displaySentence = sentences[sentenceCounter];
     }
+    
     $('#sentence').append(displaySentence);
     $(document).keypress(function(event) {
-        //console.log(document.getElementById('target-' + characterCounter));
         $("#" + (event.keyCode)).css('background-color', 'yellow');
-        if (String.fromCharCode(event.keyCode) === document.getElementById('target-' + characterCounter).innerHTML) {
+        
+        if (characterCounter === splitSentence.length) {
+            $('#sentence').empty();
+            sentenceCounter = sentenceCounter + 1;
+            splitSentence = sentences[sentenceCounter].split('');
+            console.log(sentenceCounter)
+            console.log(splitSentence)
+            var displaySentence = splitSentence.map((character, index) => {
+                return `<span id="target-${index}">${character}</span>`;
+            }).join('');
+            console.log(displaySentence)
+            $('#sentence').append(displaySentence)
+            characterCounter = 0;
+            console.log()
+        }
+         if (String.fromCharCode(event.keyCode) === document.getElementById('target-' + characterCounter).innerHTML) {
             characterCounter = characterCounter + 1;
             $('#target-' + characterCounter).css('background-color', 'yellow')
             $('#target-' + (characterCounter - 1)).css('background-color', '')
+            $('.glyphicon-thumbs-up').fadeIn();
+            $('.glyphicon-thumbs-up').fadeOut();
+            return;
+        } else {
+            $('.glyphicon-thumbs-down').fadeIn();
+            $('.glyphicon-thumbs-down').fadeOut();
+            numberOfMistakes ++;
         }
     });
     $(document).keyup(function (event)  {
